6b806a6263fb160c62c02fb41853d63387721950
João Rodrigues <joao.rodrigues@schrodinger.com>
2025-06-18 09:48:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -17,14 +17,16 @@ const people = {
 }
 
 const pallette = [
-    "#F94144",
-    "#F3722C",
-    "#F8961E",
-    "#F9C74F",
-    "#90BE6D",
-    "#43AA8B",
-    "#4D908E",
+    "#f94144",
+    "#f3722c",
+    "#f8961e",
+    "#f9844a",
+    "#f9c74f",
+    "#90be6d",
+    "#43aa8b",
+    "#4d908e",
     "#577590",
+    "#277da1",
 ]
 
 const colors = getRandomSample(pallette, Object.keys(people).length);
